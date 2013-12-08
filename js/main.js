99d3f2e424051bfb06b28224cf460467b98e5b13
allevitan <allevitan@gmail.com>
2013-12-08 10:30:55 -0500

    fix pysics and make the css nice

--- js/main.js
+++ js/main.js
@@ -2,15 +2,28 @@
 
 window.padding = 50;
 window.maxWidth = 960;
+window.tripWidth = 600;
+window.minWidth = 200;
 
 function resizeify(){
-    var w = window.innerWidth - padding*2;
-    if (w > maxWidth){
-	w = maxWidth
+    var w = window.innerWidth;
+
+    // deciding whether to engage the mobile layout
+    if (w > tripWidth){
+	document.getElementsByTagName("body")[0].classList.remove("mobile");
+    } else {
+	document.getElementsByTagName("body")[0].classList.add("mobile");
     }
-    document.getElementById("header").style.width = w + "px";
-    document.getElementById("main").style.width = w + "px";
-    document.getElementById("footer").style.width = w + "px";
+
+    // sizing the main content boxes
+    if (w > maxWidth){
+	w = maxWidth;
+    } else if (w < minWidth) {
+	w = minWidth;
+    }
+    document.getElementById("header").style.width = (w - padding*2) + "px";
+    document.getElementById("main").style.width = (w - padding*2) + "px";
+    document.getElementById("footer").style.width = (w - padding*2) + "px";
 }
 
 window.onload = function(){
